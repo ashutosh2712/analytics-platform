4574217eac5f71482ae0a180a4c36bedf5f32203
ashutosh2712 <ashu00619@gmail.com>
2026-06-04 01:47:38 +0530

    initial backend fastapi setup done

---
+++ backend/app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+#from app.database import engine, Base
+
+
+# Initialize FastAPI app
+app = FastAPI(title="Your title", version="1.0")
+
+# Allow CORS (Required for frontend communication)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust for security (use frontend URL in production)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include routes
+#app.include_router(r1.router, prefix="/r1", tags=["R1"])
+
+# Create database tables(if using)
+#Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to my first api"}
